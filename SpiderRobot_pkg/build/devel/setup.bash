2fb96de41833d6e658329b3cb0ec033134a0692c
Cody <cody@mail.example.com>
2014-11-12 22:15:28 -0600

    made clean hoping gitignore works

--- SpiderRobot_pkg/build/devel/setup.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# generated from catkin/cmake/templates/setup.bash.in
-
-CATKIN_SHELL=bash
-. "/home/xbar/groovy_workspace/Git/SpiderRobot_stack/SpiderRobot_pkg/build/devel/setup.sh"
